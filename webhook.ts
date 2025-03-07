3a7d658424ef692dd35be67e93ea451b7d940e9e
Sanmesh <sankhesanmesh@gmail.com>
2025-03-07 15:22:54 -0600

    first commit

---
+++ webhook.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// Middleware for logging requests
+app.use((req, res, next) => {
+  console.log("Incoming Request:", {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    body: req.body,
+  });
+  next();
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook request received!", req.body);
+  res.sendStatus(202);
+});
+
+app.listen(port, () => console.log(`Webhook server running on port ${port}`));
